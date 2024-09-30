fd172ee4af5dc566f6d81d378427a775304e3efb
CodeZhangBorui <zhangborui.2008@foxmail.com>
2024-09-30 10:13:58 +0800

    fix: Not display subject

--- static/current@1.0.4/issue/authors_render.js
+++ static/current@1.0.4/issue/authors_render.js
@@ -9,28 +9,28 @@ axios
     var data = result.data;
     console.log(data);
     if (data.code === 200) {
-      var leader = data.data.leader;
-      var editors = data.data.editors;
-      var respeditor = data.data.respeditor;
-      var leader_dom = document.querySelector("#leader-container");
-      var editors_dom = document.querySelector("#editors-container");
-      var respeditor_dom = document.querySelector(
-        "#respeditor-container"
-      );
-      var li = document.createElement("li");
-      li.classList.add("flex", "items-center", "mr-2");
-      li.innerText = leader;
-      leader_dom.appendChild(li);
-      editors.forEach((e) => {
-        var li = document.createElement("li");
-        li.classList.add("flex", "items-center", "mr-2");
-        li.innerText = e;
-        editors_dom.appendChild(li);
-      });
-      var li = document.createElement("li");
-      li.classList.add("flex", "items-center", "mr-2");
-      li.innerText = respeditor;
-      respeditor_dom.appendChild(li);
+      // var leader = data.data.leader;
+      // var editors = data.data.editors;
+      // var respeditor = data.data.respeditor;
+      // var leader_dom = document.querySelector("#leader-container");
+      // var editors_dom = document.querySelector("#editors-container");
+      // var respeditor_dom = document.querySelector(
+      //   "#respeditor-container"
+      // );
+      // var li = document.createElement("li");
+      // li.classList.add("flex", "items-center", "mr-2");
+      // li.innerText = leader;
+      // leader_dom.appendChild(li);
+      // editors.forEach((e) => {
+      //   var li = document.createElement("li");
+      //   li.classList.add("flex", "items-center", "mr-2");
+      //   li.innerText = e;
+      //   editors_dom.appendChild(li);
+      // });
+      // var li = document.createElement("li");
+      // li.classList.add("flex", "items-center", "mr-2");
+      // li.innerText = respeditor;
+      // respeditor_dom.appendChild(li);
       document.querySelector("#page-2-subject").innerText =
         data.data.subject[0];
       document.querySelector("#page-3-subject").innerText =
