ce84f73f24de22eb661f17f06242dd1d0ea6eca1
CodeZhangBorui <zhangborui.2008@foxmail.com>
2025-03-10 09:25:11 +0800

    feat: Delete Sentry SDK in backend

--- backend/app.py
+++ backend/app.py
@@ -34,20 +34,6 @@ app.secret_key = os.urandom(24)
 oriondb = "orion.db"
 app.config["oriondb"] = oriondb
 
-# Initalize Sentry
-if config["sentry"]["enabled"]:
-    sentry_sdk.init(
-        dsn=config["sentry"]["dsn"],
-        # Set traces_sample_rate to 1.0 to capture 100%
-        # of transactions for performance monitoring.
-        traces_sample_rate=1.0,
-        # Set profiles_sample_rate to 1.0 to profile 100%
-        # of sampled transactions.
-        # We recommend adjusting this value in production.
-        profiles_sample_rate=1.0,
-    )
-    app.config["sentry_loader_script_url"] = config["sentry"]["loader_script_url"]
-
 # Push context
 app.app_context().push()
 
